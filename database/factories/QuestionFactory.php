2822cd5e114df556fda2f930dd7647f94dec6e6e
johir-rayhan <johir.rayhan01@gmail.com>
2020-01-06 23:20:31 +0600

    update Question factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        //
+        'title' => rtrim($faker->sentence(rand(5,10)), '.'),
+        'body' => $faker->paragraphs(rand(3,7), true),
+        'views' => rand(0,10),
+        'answers' => rand(0,10),
+        'votes' => rand(-3,10),
     ];
 });
