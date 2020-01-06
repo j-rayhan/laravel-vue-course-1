ee7a3c230a622bd08c74b9e6f4ba21de390451c6
johir-rayhan <johir.rayhan01@gmail.com>
2020-01-06 21:55:52 +0600

    create relation in question model

--- app/Question.php
+++ app/Question.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+    protected $fillable = ['title', 'body'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
