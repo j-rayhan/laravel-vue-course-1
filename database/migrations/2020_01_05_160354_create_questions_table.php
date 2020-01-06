31ffbc15f7a8e500aee19181bdf3bf0a52e5ea73
johir-rayhan <johir.rayhan01@gmail.com>
2020-01-06 21:45:54 +0600

    Update questions table

--- database/migrations/2020_01_05_160354_create_questions_table.php
+++ database/migrations/2020_01_05_160354_create_questions_table.php
@@ -15,7 +15,17 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->uniqid();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedBigInteger('best_answer_id')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
