2d58f3d988322cc9c0671c81446516e6355d5f01
johir-rayhan <johir.rayhan01@gmail.com>
2020-01-07 23:37:47 +0600

    change style

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,10 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">All Questions</div>
-
                 <div class="card-body">
                     @foreach ($questions as $question)
                      <div class="media">
@@ -19,7 +18,9 @@
                      </div>
                      <hr/>
                     @endforeach
-                    {{$questions->links()}}
+                    <div class="text-cente">
+                     {{$questions->links()}}
+                    </div>
                 </div>
             </div>
         </div>
