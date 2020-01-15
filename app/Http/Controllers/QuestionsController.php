d74b6a0311d9ac056679d54f08d1df1d1f5f42fe
johir-rayhan <johir.rayhan01@gmail.com>
2020-01-15 22:25:25 +0600

    [ADD] debug in Questions Contorller

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,8 +14,10 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
-        return view('questions.index', compact('questions'));
+        \DB::enableQueryLog();
+        $questions = Question::with('user')->latest()->paginate(10);
+        view('questions.index', compact('questions'))->render();
+        dd(\DB::getQueryLog());
     }
 
     /**
